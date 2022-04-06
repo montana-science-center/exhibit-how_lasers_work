f372dc22900e07c9e9b04fe7a644c66cf3839942
Kevin <kevinw@virtualmatter.org>
2022-04-06 00:30:01 -0400

    Adjustments after first hardware testing

--- laser_mirror/laser_mirror.ino
+++ laser_mirror/laser_mirror.ino
@@ -1,26 +1,26 @@
-#include <FastLED.h>        // 3.5.0
 #include <AccelStepper.h>   // 1.61.0
 
 
 // software config
-const byte sleep_delay_s = 1;         // seconds to wait before putting stepper in low-current mode.
-const byte laser_off_delay_m = 30;    // minutes to wait before turning off the laser
-const int stepper_max_steps = 200;    // number of steps controled by the knob
-const int stepper_reset_steps = -300; // number of steps to over-travel to ensure mechanical stop is hit
-const int stepper_speed = 1000;       // steps per second
-const int stepper_accel = 1000;       // steps per second^2
-const int knob_sample_ms = 50;        // number of milliseconds betwene knob reads
-const int knob_samples = 10;          // number of knob reads to average
-const int knob_deadband = 8;          // ADC threshold before exhibit is considered "active"
+const byte sleep_delay_s = 1;      // seconds to wait before putting stepper in low-current mode.
+const byte laser_off_delay_m = 1;      // minutes to wait before turning off the laser
+const int stepper_max_steps = 1424;    // number of steps controled by the knob
+const int stepper_reset_steps = -1500; // number of steps to ensure mechanical stop is hit
+const int stepper_speed = 2000;        // steps per second
+const int stepper_reset_speed = 300;   // steps per second during reset
+const int stepper_accel = 8000;        // steps per second^2
+const int knob_sample_ms = 1;          // number of milliseconds betwene knob reads
+const int knob_samples = 10;           // number of knob reads to average
+const int knob_deadband = 8;           // ADC threshold before exhibit is considered "active"
 
 
 // hardware config
-const int adc_res = 1024;
+const float adc_res = 1011.0; // ensure float division
 const byte pin_step = 3;
 const byte pin_dir = 4;
 const byte pin_knob = A0;
 const byte pin_motor_power = 7;
-const byte pin_laser_power = 6;
+const byte pin_laser_power = 8;
 AccelStepper stepper(AccelStepper::DRIVER, pin_step, pin_dir);
 
 
@@ -35,36 +35,55 @@ bool awake = 1;
 long last_touch_ms = 0;
 
 
-void setup() {
-    pinMode(pin_motor_power, OUTPUT);
-    pinMode(pin_laser_power, OUTPUT);
+void set_power(bool state) {
+    digitalWrite(pin_laser_power, state);
+    power = state;
+}
 
-    // configure stepper
-    stepper.setMaxSpeed(stepper_speed);
-    stepper.setAcceleration(stepper_accel);
 
-    // init knob sample array to zero
-    memset(knob_buffer, 0, sizeof(knob_buffer));
-
-    digitalWrite(pin_motor_power, HIGH);
-    digitalWrite(pin_laser_power, HIGH);
-
-    reset_stepper();
+void set_awake(bool state) {
+    // enable pin pulled high turns off the motor.
+    digitalWrite(pin_motor_power, !state);
+    awake = state;
 }
 
 
 void reset_stepper() {
+    stepper.setAcceleration(stepper_accel);
+    
+    // set reset speed
+    stepper.setMaxSpeed(stepper_reset_speed);
+    
     // blocking call to reset the stepper position
     stepper.runToNewPosition(stepper_reset_steps);
     stepper.setCurrentPosition(0);
+    
+    // set normal running speed
+    stepper.setMaxSpeed(stepper_speed);
+}
+
+
+void setup() {
+
+    Serial.begin(9600);
+
+    // init knob sample array to zero
+    memset(knob_buffer, 0, sizeof(knob_buffer));
+
+    pinMode(pin_motor_power, OUTPUT);
+    pinMode(pin_laser_power, OUTPUT);
+    set_power(1);
+    set_awake(1);
+
+    // configure stepper
+    reset_stepper();
 }
 
 
 void loop() {
 
-    EVERY_N_MILLISECONDS(knob_sample_ms) {
-        sample_knob();
-    }
+    sample_knob();
+    //Serial.println(knob_resolved_value);
 
     if (power && awake) {
         update_position();
@@ -81,29 +100,23 @@ void update_state() {
         knob_sleep_value = knob_resolved_value;
 
         if (!power) {
-            digitalWrite(pin_laser_power, HIGH);
-            power = 1;
+            set_power(1);
         }
-        
         if (!awake) {
-            digitalWrite(pin_motor_power, HIGH);
-            awake = 1;
+            set_awake(1);
         }
-        
         return;
     }
-    
+
     if (awake) {
         if (millis() - last_touch_ms > sleep_delay_s * 1000) {
-            digitalWrite(pin_motor_power, LOW);
-            awake = 0;
+            set_awake(0);
         }
     }
 
     if (power) {
         if (millis() - last_touch_ms > laser_off_delay_m * 60000) {
-            digitalWrite(pin_laser_power, LOW);
-            power = 0;
+            set_power(0);
         }
     }
 }
@@ -120,12 +133,12 @@ void sample_knob() {
     knob_buffer_total = knob_buffer_total - knob_buffer[knob_buffer_read_index];
     knob_buffer[knob_buffer_read_index] = analogRead(pin_knob);
     knob_buffer_total = knob_buffer_total + knob_buffer[knob_buffer_read_index];
-    
+
     knob_buffer_read_index ++;
 
     if (knob_buffer_read_index >= knob_samples) {
         knob_buffer_read_index = 0;
     }
-    
+
     knob_resolved_value = knob_buffer_total / knob_samples;
 }
